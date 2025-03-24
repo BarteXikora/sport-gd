6dd1184e55f6617d7fea1e2c04fb2e06a6e7cae1
Bartłomiej Sikora <sbartek50@gmail.com>
2025-03-24 19:10:18 +0100

    Used layout components in the app index.

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,17 @@
 import { Text, View } from 'react-native'
+import { AppScreen, MainScroll, FixedBottom } from '@/components/layout/ScreenLayout'
 
 const Index = () => {
   return (
-    <View>
-      <Text>sport gd</Text>
-    </View>
+    <AppScreen>
+      <MainScroll>
+        <Text className='color-white text-app-default'>Hello, World!</Text>
+      </MainScroll>
+
+      <FixedBottom>
+        <Text className='color-white'>I am footer 👉👈</Text>
+      </FixedBottom>
+    </AppScreen>
   )
 }
 
