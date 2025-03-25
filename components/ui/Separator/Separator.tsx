aaf31e74b46cf5ea736213394fdc9d570dc61c19
Bartłomiej Sikora <sbartek50@gmail.com>
2025-03-25 13:34:38 +0100

    Created the Separator component.

---
+++ components/ui/Separator/Separator.tsx
@@ -0,0 +1,9 @@
+import { View } from 'react-native'
+
+const Separator = () => {
+    return <View className='my-[32px] items-center'>
+        <View className='h-[3px] w-[48px] rounded-app bg-gray-500' />
+    </View>
+}
+
+export default Separator
